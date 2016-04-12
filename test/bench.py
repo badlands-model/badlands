28ec4f957bb1e90bc320d9a38ff99eb64285436a
Ian Howson <ian@mutexlabs.com>
2016-04-12 15:10:24 +1000

    Generate benchmark times for 1..4 cores.

--- test/bench.py
+++ test/bench.py
@@ -10,12 +10,11 @@ model.load_xml('bench/input.xml')
 model.run_to_time(4000)
 print 'singlethread finished in %s seconds' % (time.time() - start_time)
 
-start_time = time.time()
-remote = RemoteModel()
-# match the child's cwd to ours
-remote._view.execute('import os; os.chdir("%s")' % os.getcwd())
-remote.load_xml('bench/input.xml')
-print 'load time is %s' % time.time()
-remote.run_to_time(4000)
-print 'run time is %s' % time.time()
-print 'mpi finished in %s seconds' % (time.time() - start_time)
+for ncpus in range(1, 5):
+    start_time = time.time()
+    remote = RemoteModel(maxcpus=ncpus)
+    # match the child's cwd to ours
+    remote._view.execute('import os; os.chdir("%s")' % os.getcwd())
+    remote.load_xml('bench/input.xml')
+    remote.run_to_time(4000)
+    print 'mpi (%d cores) finished in %s seconds' % (ncpus, time.time() - start_time)
